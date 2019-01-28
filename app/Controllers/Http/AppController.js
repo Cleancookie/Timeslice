cace98880be1da08060a73f77533e448c8c60e68
Alex Law <pokemon999101@hotmail.co.uk>
2019-01-28 22:00:49 +0000

    🔥 Removing initial testing code

--- app/Controllers/Http/AppController.js
+++ app/Controllers/Http/AppController.js
@@ -4,10 +4,6 @@ class AppController {
   async home({req, res, view}) {
     return view.render('homepage');
   }
-
-  async handle() {
-    console.log('yerd')
-  }
 }
 
 module.exports = AppController
--- app/Controllers/Http/PublicController.js
+++ app/Controllers/Http/PublicController.js
@@ -2,9 +2,6 @@
 var AppController = require('./AppController');
 
 class PublicController extends AppController {
-  async home({req, res, view}) {
-    return super.home({req, res, view})
-  }
 }
 
 module.exports = PublicController
