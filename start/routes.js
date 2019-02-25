ad363bd69b864c0762c96a2828faddbf9d1b475a
Alex Law <pokemon999101@hotmail.co.uk>
2019-02-25 23:35:15 +0000

    🛠 Group api routes

--- start/routes.js
+++ start/routes.js
@@ -18,12 +18,15 @@ const Route = use('Route')
 
 Route.get('/', 'PublicController.home')
 
-Route.post('api/login', 'UserController.login').as('user.login')
-Route.post('api/logout', 'UserController.logout').as('user.logout')
-Route.post('api/register', 'UserController.register').as('user.register')
+Route.group(()=>{
+  Route.post('login', 'UserController.login').as('user.login')
+  Route.post('logout', 'UserController.logout').as('user.logout')
+  Route.post('register', 'UserController.register').as('user.register')
 
-Route.get('api/projects', 'ProjectController.index').as('project.index').middleware('auth')
-Route.get('api/projects/:id', 'ProjectController.read').middleware('auth')
-Route.post('api/projects', 'ProjectController.create').as('project.create').middleware('auth')
-Route.post('api/projects/:id', 'ProjectController.update').middleware('auth')
-Route.delete('api/projects/:id', 'ProjectController.delete').middleware('auth')
+  Route.get('projects', 'ProjectController.index').as('project.index').middleware('auth')
+  Route.get('projects/:id', 'ProjectController.read').middleware('auth')
+  Route.post('projects', 'ProjectController.create').as('project.create').middleware('auth')
+  Route.post('projects/:id', 'ProjectController.update').middleware('auth')
+  Route.delete('projects/:id', 'ProjectController.delete').middleware('auth')
+}).prefix('api/v1')
+
