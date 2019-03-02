8be20b0414dd4d3a387e8c41625182d7da9c9f8c
Alex Law <pokemon999101@hotmail.co.uk>
2019-03-02 14:25:25 +0000

    ✏ Add comments to routes

--- start/routes.js
+++ start/routes.js
@@ -19,22 +19,25 @@ const Route = use('Route')
 Route.get('/', 'PublicController.home')
 
 Route.group(()=>{
+  // Login API
   Route.post('login', 'UserController.login').as('user.login')
   Route.post('logout', 'UserController.logout').as('user.logout')
   Route.post('register', 'UserController.register').as('user.register')
 
+  // Projects API
   Route.get('projects', 'ProjectController.index').as('project.index').middleware('auth')
   Route.get('projects/:id', 'ProjectController.show').middleware('auth')
   Route.post('projects', 'ProjectController.create').as('project.create').middleware('auth')
   Route.post('projects/:id', 'ProjectController.update').middleware('auth')
   Route.delete('projects/:id', 'ProjectController.delete').middleware('auth')
 
+  // Tasks API
   Route.get('projects/:id/tasks', 'TaskController.index').middleware('auth')
   Route.get('tasks/:id', 'TaskController.show').middleware('auth')
   Route.post('projects/:id/tasks', 'TaskController.create').middleware('auth')
   Route.post('tasks/:id', 'TaskController.update').middleware('auth')
   Route.post('tasks/:id/delete', 'TaskController.delete').middleware('auth')
-
+  // Task actions
   Route.post('tasks/:id/update-user', 'TaskController.updateUser')
   Route.post('tasks/:id/update-stage', 'TaskController.updateStage')
 }).prefix('api/v1')
