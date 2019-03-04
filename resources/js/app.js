c1999a75f7046ae3f78d7819c0e7bb2ea83545d2
Alex Law <pokemon999101@hotmail.co.uk>
2019-03-04 12:30:45 +0000

    🔥 Remove all front end views

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,3 @@
 window.$ = require('jquery')
 import 'bootstrap'
 window.axios = require('axios')
-
-// Components
-import DashboardComponent from './components/dashboard/index'
-
-// Init
-window.DashboardComponent = new DashboardComponent()
-window.DashboardComponent.init()
-
